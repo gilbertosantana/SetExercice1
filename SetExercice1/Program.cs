12bd529b72b9d4417adf1e6045ee268bda86240d
gilbertosantana <gilbertosantoss307@gmail.com>
2022-11-27 15:49:11 -0300

    exercício proprosto utilizando sortedset

---
+++ SetExercice1/Program.cs
@@ -0,0 +1,32 @@
+﻿SortedSet<int> cursoA = new SortedSet<int>();
+SortedSet<int> cursoB = new SortedSet<int>();
+SortedSet<int> cursoC = new SortedSet<int>();
+
+Console.Write("How many students for course A? ");
+int quant = int.Parse(Console.ReadLine()!);
+for(int i = 0; i < quant; i++)
+{
+    int codigo = int.Parse(Console.ReadLine()!);
+    cursoA.Add(codigo);
+}
+
+Console.Write("How many students for course B? ");
+quant = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < quant; i++)
+{
+    int codigo = int.Parse(Console.ReadLine()!);
+    cursoB.Add(codigo);
+}
+
+Console.Write("How many students for course C? ");
+quant = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < quant; i++)
+{
+    int codigo = int.Parse(Console.ReadLine()!);
+    cursoC.Add(codigo);
+}
+
+SortedSet<int> todos = new SortedSet<int>(cursoA);
+todos.UnionWith(cursoB);
+todos.UnionWith(cursoC);
+Console.WriteLine("Total students: " + todos.Count);
